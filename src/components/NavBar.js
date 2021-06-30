7f50d32080e0f410ddadf63aa2709dfbd24ace10
Arturo P <arturoperezfigueroa@gmail.com>
2021-06-29 22:33:56 -0500

    Inicio componente NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return(
+        <nav className="NavBar">
+            <p className="logo">MyStore</p>
+        </nav>
+    );
+}
+
+export default NavBar;
