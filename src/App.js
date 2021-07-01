59e11c8f7f3f806e77d5e40cb7743aa2b0f5305c
Arturo P <arturoperezfigueroa@gmail.com>
2021-07-01 09:15:48 -0500

    Creacion componente Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Card />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NavBar.css';
 
+
 function NavBar() {
     return(
         <nav className="NavBar">
             <p className="logo">MyStore</p>
+            <input className="input-search"></input>
+            <p className="login">login</p>
         </nav>
     );
 }
